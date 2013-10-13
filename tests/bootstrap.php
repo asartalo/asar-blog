4a831c33fc5c15f0a6584f2cc9c9835df8016de2
asartalo <asartalo@gmail.com>
2013-10-13 08:06:16 +0800

    Updating email address.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 /**
 * This file is part of the Asar Blog
 *
-* (c) Wayne Duran <asartalo@projectweb.ph>
+* (c) Wayne Duran <asartalo@gmail.com>
 *
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
